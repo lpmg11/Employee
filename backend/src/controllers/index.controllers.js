134043b779e71edc67bf1b5a5c3659c1876b5e84
lpmg11 <lpmg11manrique@gmail.com>
2023-06-23 12:44:58 -0600

    Adding backend in docker compose

--- backend/src/controllers/index.controllers.js
+++ backend/src/controllers/index.controllers.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-    port:'5432'
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "postgres",
+    port: process.env.DB_PORT || "5432"
 })
 
 const getEmployees = async(req,res) => {
